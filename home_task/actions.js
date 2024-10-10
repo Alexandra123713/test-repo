ea3645368a0e778a522866feeb1c6aed08f13d5d
Alexandra Cerevatii <alexandracerevatii@gmail.com>
2024-10-10 14:05:39 +0300

    Added store and css file

--- home_task/actions.js
+++ home_task/actions.js
@@ -68,6 +68,7 @@ const operation = document.querySelector('select')
 
 const result = document.getElementById('result')
 const resultSpan = document.getElementById('result-span')
+resultSpan.id = 'resultSpan'
 
 const resultButtonHandler = function () {
   const selectedOperation = operation.value;  
@@ -103,22 +104,31 @@ result.addEventListener('click', resultButtonHandler)
 
 //Table
 
-const table = document.getElementById('table-container')
-const n = 20
+const table = document.getElementById('tableContainer')
+const n = 10
 
 for(let i=0; i<n; i++) {
+    let disabledString = '';
+    if (i === 0) {
+        disabledString = 'disabled'
+        
+    }
+
     table.insertAdjacentHTML('beforeend', 
-    `<div class="cell" style="display: flex; flex-direction: row; justify-content: space-around; padding: 5px; border: 1px solid black">
+    `<div class="cell">
         <span class="row-text">Row ${i+ 1}</span>
         <button class="remove">Delete row</button>
-        <button class="up">Up</button>
+        <button class="up row-${i+ 1}" ${disabledString}>Up</button>
     </div>`
     )
 }
 
+const firstButton = document.getElementsByClassName('row-1')[0]
+firstButton.style.cursor = 'default'
+
 // table.addEventListener('click', function(event) {
 //     if(event.target.classList.contains('remove')){
-//         event.target.parentNode.remove()
+//            </div>event.target.parentNode.remove()
 //     }
 // })
 
@@ -126,9 +136,23 @@ for(let i=0; i<n; i++) {
 const removeButtons = document.querySelectorAll('.remove')
 
 removeButtons.forEach(removeButton => {
+
     removeButton.addEventListener('click', function() {
         this.parentNode.remove()
-})})
+
+        const allUpButtons = document.querySelectorAll('.up')
+        allUpButtons.forEach(button => {
+            button.disabled = false
+            button.style.cursor = 'pointer'
+        })
+        const topRow = table.firstElementChild
+        const topUpButton = topRow.querySelector('.up')
+        topUpButton.disabled = true;
+        topUpButton.style.cursor = 'default'
+    })
+})
+
+
 
 const upButtons = document.querySelectorAll('.up')
 
@@ -137,17 +161,148 @@ upButtons.forEach(upButton => {
     upButton.addEventListener('click', function() {
         const entireRow = this.parentNode
         table.prepend(entireRow)
+
+        const allUpButtons = document.querySelectorAll('.up')
+        allUpButtons.forEach(button => {
+            button.disabled = false
+            button.style.cursor = 'pointer'
+        })
+        const topRow = table.firstElementChild
+        const topUpButton = topRow.querySelector('.up')
+        topUpButton.disabled = true;
+        topUpButton.style.cursor = 'default'
     })
+    
 })
+
+
+
 
 
 const cells = document.querySelectorAll('.cell')
 
 cells.forEach(cell => {
     cell.addEventListener('mouseover', function() {
-        cell.setAttribute('class', 'colored')
+        cell.style.backgroundColor = '#fae79a';
     })
     cell.addEventListener('mouseout', function() {
-        cell.removeAttribute('class', 'colored')
+        cell.style.backgroundColor = '';
     })
 })
+
+
+// Store
+
+const items = [
+    {
+    title: 'Hoco BS60 Exploring sports BT speaker, black',
+    price: 199,
+    imgSource: './assets/BS60.png'
+    },
+    {
+    title: 'Borofone BR28 Joyful sports BT speaker, gray',
+    price: 259,
+    imgSource: './assets/BR28.png'
+    },
+    {
+    title: 'Hoco X82 iP silicone charging data cable, white',
+    price: 149,
+    imgSource: './assets/X82.png'
+    },
+    {
+    title: 'Borofone BQ20 Wings multifunctional wireless fast charger, black',
+    price: 299,
+    imgSource: './assets/BQ20.png'
+    },
+    {
+    title: 'Hoco EW51 True wireless ANC noise reduction BT headset, white',
+    price: 789,
+    imgSource: './assets/EW51.png'
+    },
+    {
+    title: 'Borofone BH40 Kyle air outlet magnetic car holder, black',
+    price: 199,
+    imgSource: './assets/BH40.png' 
+    },  
+]
+
+
+
+function renderItems () {
+    const container = document.getElementById('storeContainer');
+
+    items.forEach((item, index) => {
+        container.insertAdjacentHTML('beforeend', `
+        <div class="item-container">
+            <img class="store-image" src="${item.imgSource}" />
+            <h4 class='item-title'>${item.title}</h4>
+                 <div class="priceWishListButton">
+                     <span class='item-price'>${item.price} lei</span>
+                     <button class='add-to-wishlist' data-button-id="${index}">Add to wishlist</button>
+                 </div>
+        </div>
+        `)
+    })
+}
+
+renderItems()
+
+const wishlistContainer = document.getElementById('wishlistContainer')
+let totalButtonClicks = 0;
+
+function addToWishlist () {
+    totalButtonClicks ++;
+    wishlistContainer.style.padding = 0
+
+    const emptyText = document.getElementById('emptyList');
+    emptyText.style.display = 'none'
+    const title = this.parentNode.parentNode.getElementsByClassName('item-title')[0];
+    const addedTitle = title.innerText;
+    const price = this.parentNode.getElementsByClassName('item-price')[0];
+    const addedPrice = price.innerText;
+    const image = this.parentNode.parentNode.getElementsByClassName('store-image')[0];
+    const addedImage = image.src;
+    const button = this;
+
+    wishlistContainer.insertAdjacentHTML('afterBegin', `
+    <div class="wish-item" data-button-id="${button.dataset.buttonId}">
+      <img class="added-store-image" src="${addedImage}" />
+      <h4 style="width: 30%">${addedTitle}</h4>
+      <span>${addedPrice}</span>
+      <button class="wishlist-delete">Delete</button>
+    </div>
+    `);
+    if(totalButtonClicks >= 1) {
+        button.disabled = true
+        button.textContent = 'Added to wishlist'
+    }
+ 
+    const deleteButtons = document.getElementsByClassName('wishlist-delete')
+    Array.from(deleteButtons).forEach(deleteButton => {
+             deleteButton.addEventListener('click', function () {
+                const wishItem = this.parentNode;
+                const buttonId = wishItem.dataset.buttonId;
+                const originalButton = document.querySelector(`button[data-button-id="${buttonId}"]`);
+
+                originalButton.disabled = false;
+                originalButton.textContent = 'Add to wishlist';
+
+                wishItem.remove()
+
+                  if(deleteButtons.length === 0){
+                containerIsEmpty = true;
+                emptyText.style.display = 'block'
+                wishlistContainer.style.padding = "2rem"
+             }
+        
+    })
+})
+}
+
+const addToWishlistButtons = document.getElementsByClassName('add-to-wishlist')
+
+Array.from(addToWishlistButtons).forEach(button => {
+    button.addEventListener('click', addToWishlist)
+})
+
+
